7d601038ca8f97d41e4effb6efa7a68865de26e7
Matheus <148924177+MatheusdCastro@users.noreply.github.com>
2023-11-08 22:58:35 -0300

    Update VectorMusica.java

--- Spotifly/src/repository/VectorMusica.java
+++ Spotifly/src/repository/VectorMusica.java
@@ -1,5 +1,22 @@
 package repository;
 
-public class RepositorioMusica {
+import java.util.Vector;
+import Spotifly.src.models.*;
 
+public class VectorMusica implements IRepositorio{
+   private Vector<Musica> musicas;
+
+   public VectorMusica(){
+    this.musicas = new Vector<Musica>();
+   }
+
+   public Musica buscar(String musica){
+      for(Musica music : this.musicas){
+         if(music.getNome().equals(musica)){
+            return music;
+         }
+      }
+      return null;
+   }
 }
+
