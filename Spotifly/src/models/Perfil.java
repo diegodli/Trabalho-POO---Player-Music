f73ae73d7f4cc145554f1e20d31bcb10601e4928
Victor Guedes <137237707+victorguedes2001@users.noreply.github.com>
2023-11-08 22:59:44 -0300

    Atualização do perfil

--- Spotifly/src/models/Perfil.java
+++ Spotifly/src/models/Perfil.java
@@ -1,5 +1,29 @@
 package models;
-
-public class Perfil {
-
+// import IRepositorioMusica
+public abstract class Perfil {
+    private String nomeDoPerfil;
+    private String email;
+    private IRepositorioMusica playlists;
+    
+    public Perfil (String nomeDoPerfil, String email, IRepositorioMusica playlists) {
+        this.nomeDoPerfil = nomeDoPerfil;
+        this.email = email;
+        this.playlists = playlists;
+    }
+    
+    public abstract void CriarPlaylist(String nomePlaylist);
+    
+    public String getNomeDoPerfil() {
+        return nomeDoPerfil;
+    }
+    public void setNomeDoPerfil(String nomeDoPerfil) {
+        this.nomeDoPerfil = nomeDoPerfil;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
