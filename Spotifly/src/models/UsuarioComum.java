8923d533819c41c3e6271b797d666840661f8b3d
victorguedes2001 <victorguedes80@alu.ufc.br>
2023-11-25 07:38:34 -0300

    Usuario

--- Spotifly/src/models/UsuarioComum.java
+++ Spotifly/src/models/UsuarioComum.java
@@ -7,14 +7,14 @@ import java.io.Serializable;
 
 public class UsuarioComum extends Usuario implements Serializable {
 
-    private String username;
-    private String password;
+    /*private String username;
+    private String password;*/
 
     public UsuarioComum(String username, String password) {
         super(username, password);
     }
 
-    // Métodos para controle da serialização
+    /*// Métodos para controle da serialização
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
         out.writeObject(getUsername());
@@ -28,7 +28,7 @@ public class UsuarioComum extends Usuario implements Serializable {
         String password = (String) in.readObject();
         this.username = username;  
         this.password = password;
-    }
+    }*/
 
 
     // Implementação específica para usuário comum
