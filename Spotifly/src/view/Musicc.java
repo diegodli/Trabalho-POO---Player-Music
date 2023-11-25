48e012b954615f1c647f87ec2271e3d776a8c88a
victorguedes2001 <victorguedes80@alu.ufc.br>
2023-11-25 07:56:46 -0300

    Revert "Usuario"
    This reverts commit b42b008629a824b0547acc955d38d372b5d24a92.

--- Spotifly/src/view/Musicc.java
+++ Spotifly/src/view/Musicc.java
@@ -3,15 +3,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import models.Usuario;
-import models.UsuarioPremium;
 
 public class Musicc extends JFrame {
-    
-    private Usuario usuario;
 
-    public Musicc(Usuario usuario) {
-        this.usuario = usuario;
+    public Musicc() {
         // Configurações do frame
         setTitle("Musicas");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,42 +19,31 @@ public class Musicc extends JFrame {
         panel.setBackground(Color.DARK_GRAY);
         add(panel);
 
-        
         // Adiciona o título "Musicas" no topo
         JLabel titleLabel = new JLabel("Musicas");
         titleLabel.setForeground(Color.WHITE);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        panel.add(titleLabel, BorderLayout.PAGE_START);
-        
-        JLabel userInfoLabel = new JLabel("Usuário: " + usuario.getUsername() + " (" + tipoUsuario() + ")");
-        userInfoLabel.setForeground(Color.WHITE);
-        userInfoLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-        panel.add(userInfoLabel, BorderLayout.BEFORE_LINE_BEGINS);
+        panel.add(titleLabel, BorderLayout.NORTH);
 
-        // Configuração do botão de volta à página inicial 
+        // Configuração do botão de volta à página inicial no canto superior direito
         JButton backButton = new JButton("Voltar");
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new PaginaInicial(usuario).setVisible(true);
+                new PaginaInicial().setVisible(true);
                 dispose(); // Fecha o frame atual (Music)
             }
         });
 
-        panel.add(backButton, BorderLayout.SOUTH);
+        panel.add(backButton, BorderLayout.PAGE_START);
     }
-    
-    private String tipoUsuario() {
-        if (usuario instanceof UsuarioPremium) return "Premium";
-        else return "Comum";
-    }
-    
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new TelaLogin().setVisible(true);
+                new Musicc().setVisible(true);
             }
         });
     }
